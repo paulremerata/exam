bbc9517c30bf6b14f8b8cfcb496e98793def2669
Paul Christian Remerata <paul26rem@gmail.com>
2016-04-19 15:51:37 +0800

    first commit

---
+++ first code.py
@@ -0,0 +1,13 @@
+random_numbers = [76,23,1,45,7,5,2,1,2,2,2,4,88,66,66,55]
+def remove_duplicates(x):
+    l=[]
+    c=True
+    for i in x:
+        for j in l:
+            if i==j:
+                c=False
+        if c:
+            l.append(i)
+        c=True
+    return l
+print str(sorted(remove_duplicates(random_numbers)))
