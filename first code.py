fd47807fb625a2db986be30a1f0e11c8f8349831
Paul Christian Remerata <paul26rem@gmail.com>
2016-04-19 16:06:17 +0800

    Add an adder

--- first code.py
+++ first code.py
@@ -1,4 +1,4 @@
-random_numbers = [76,23,1,45,7,5,2,1,2,2,2,4,88,66,66,55]
+random_numbers = [76,23,1,45,7,5,2,1,2,2,2,4,88,66,66,55,5,12,314,545,777,12,32]
 def remove_duplicates(x):
     l=[]
     c=True
@@ -11,3 +11,7 @@ def remove_duplicates(x):
         c=True
     return l
 print "Sorted and Purified "+str(sorted(remove_duplicates(random_numbers)))
+
+x=int(raw_input("first number"))
+y=int(raw_input("2nd number"))
+print x+y
